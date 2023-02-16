426a10970484543d9bd53c278ab891f10254e922
MohamedElKh <mohamedkharashy27@yahoo.com>
2023-02-16 10:24:26 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,14 @@
-
 #include <stdio.h>
 
-
 /**
- * function_name - to prints exactly Programming is like building a multilingual puzzle.
+ * print - Programming is like building a multilingual puzzle.
  * @parameter : void
  * return : zero value
  */
 
-int main(void) 
+int main(void)
+
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming\tis\tlike\tbuilding\ta\tmultilingual\tpuzzle");
         return(0);
 }	
