6eaae5040f646a61dd51614e90d6d68af90aa44a
MohamedElKh <mohamedkharashy27@yahoo.com>
2023-02-16 10:29:12 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,8 +7,7 @@
  */
 
 int main(void)
-
 {
-	puts("\"Programming\tis\tlike\tbuilding\ta\tmultilingual\tpuzzle");
+	puts("\"Programming is like building a multilingual puzzle");	
         return(0);
 }	
