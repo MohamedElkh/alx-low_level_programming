a09e3bc97410c9ae9b9d70330f5788569a36f871
MohamedElKh <mohamedkharashy27@yahoo.com>
2023-02-16 08:38:32 -0800

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Write a C program that prints exactly "Programming is like building a multilingual puzzle", followed by a new line.
+ * Your program should end with the value 0.
+ */
+
+int main(void) 
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+};	
