0a1b7b885702ead7f0fea327e40e4060ec77f1b8
MohamedElKh <mohamedkharashy27@yahoo.com>
2023-02-17 06:52:51 -0800

    3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,14 +13,14 @@ int main(void)
 	char a ;
 	char A ;
 
-	a = 'a';
-	A = 'A';
-	while(a <= 'z')
+	a ='a';
+	A ='A';
+	while (a <= 'z')
 	{
 		putchar(a);
 		a++;
 	}
-	while(A <= 'Z')
+	while (A <= 'Z')
 	{
 		putchar(A);
 		A++;
