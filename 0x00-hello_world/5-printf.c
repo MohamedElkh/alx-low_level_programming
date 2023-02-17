835aa8476e35e899e38f64ce5435aafb4befff86
MohamedElKh <mohamedkharashy27@yahoo.com>
2023-02-16 16:13:57 -0800

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print sentence with proper grammar, but the outcome is a piece of art
+ * followed by a new line.
+ * @void: nothing .
+ *
+ * Description: print with proper grammar, but the outcome is a piece of art
+ * Return: 0 succed
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
