a6904a82f358858bd5ee250c4cbaa3ca42828dbd
MohamedElKh <mohamedkharashy27@yahoo.com>
2023-02-17 11:11:01 -0800

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints the numbers from 0 to 9 
+ * @void: nothing .
+ *
+ * Description: prints the numbers from 0 to 9 by putchar
+ * Return: 0 Always .
+ */
+
+int main(void)
+{
+	int i;
+
+	while (i < 10)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+
+}
