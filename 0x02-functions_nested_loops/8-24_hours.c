3316299edc1cde068fba27abbfe4e44272875f62
MohamedElKh <mohamedkharashy27@yahoo.com>
2023-02-21 10:51:25 -0800

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,20 +10,29 @@
 
 void jack_bauer(void)
 {
-	int i,j;
+	int i,j,e,r;
 
-	for (i = 0; i <= 24; i++)
+	for (i = 0; i <= 2; i++)
 	{
-		for (j = 0; j <= 60; j++)
-		{
-			_putchar((i / 10) + '0');
-			 _putchar((i % 10) + '0');
-			  _putchar(':');
-			   _putchar((j / 10) + '0');
-			    _putchar((j % 10) + '0');
-			     _putchar('\n');
-
-		}
-
+	for (j = 0; j <= 9; j++)
+	{
+	if((i <= 1 && j <= 9) || (i <= 2 && j <= 3))
+	{
+	for (e = 0; e <= 5; e++)
+	{
+	for (r = 0 ; r <= 9; r++)
+	{
+	_putchar(i + '0');
+	_putchar(j  + '0');
+	_putchar(58);
+	_putchar(e + '0');
+	_putchar(r  + '0');
+	_putchar('\n');
 	}
+	}
+	}
+	}
+}
+
+
 }
