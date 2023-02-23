7d4bf06e258ac2fa52c409b870b3353cebad92d7
MohamedElKh <mohamedkharashy27@yahoo.com>
2023-02-23 05:48:19 -0800

    0

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints if variable n greater or equal or less than 0.
+ * @void: nothing .
+ *
+ * Description: prints if variable n greater or equal or less than 0
+ * Return: 0 Always .
+ */
+
+int main(void)
+{
+        int n;
+
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        /* your code goes there */
+        if (n > 0)
+        {
+                printf("%d is positive\n", n);
+        }
+        else if (n == 0)
+        {
+                printf("%d is zero\n", n);
+        }
+        else
+        {
+                printf("%d is negative\n", n);
+        }
+        return (0);
+
+}
