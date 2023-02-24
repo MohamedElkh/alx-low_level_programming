105edea5e893fcbb686aa0f016fcb532be5ec7ed
MohamedElKh <mohamedkharashy27@yahoo.com>
2023-02-24 06:10:25 -0800

    7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -4,7 +4,7 @@
  * print_diagonal - a function that draws a diagonal line.
  * @n: the character to be checked.
  *
- * Description:a function that draws a diagonal line 
+ * Description:a function that draws a diagonal line
  * on the terminal.
  * Return: nothing.
  */
@@ -12,23 +12,23 @@
 void print_diagonal(int n)
 {
 
-	int a, i;
+int a, i;
 
-	if(n <= 0)
-	{
-		_putchar('\n');
-	}
-	else 
-	{
-		for (i = 1; i <= n; i++)
-		{
-			for (a = 1; a < i; a++)
-			{
-				_putchar(' ');
-			}
-			_putchar('\\');
-			_putchar('\n');
-		}
-	}	
+if(n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 1; i <= n; i++)
+{
+for (a = 1; a < i; a++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
 
 }
