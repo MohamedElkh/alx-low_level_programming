63381b9e0b83e16c77a418a06b19f373e7d65264
MohamedElKh <mohamedkharashy27@yahoo.com>
2023-03-02 04:00:02 -0800

    8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * rot13 - a function that encodes a string using rot13.
+ * @q: the pointer to be checked.
+ *
+ * Description: a function that encodes a string using rot13.
+ * Return: the result of q.
+ */
+
+char *rot13(char *q)
+{
+	int a, i;
+
+	char input [] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char output [] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (a = 0; q[a] != '\0'; a++)
+	{
+		for (i = 0; input[i] != '\0'; i++)
+		{
+			if (q[a] == input[i])
+			{
+				q[a] = output[i];
+			}
+		}
+	}
+	return (q);
+
+
+}
