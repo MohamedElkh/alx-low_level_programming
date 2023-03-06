267461712036e7cf6d53638d71527aaa7dca92d4
MohamedElKh <mohamedkharashy27@yahoo.com>
2023-03-06 04:03:17 -0800

    3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,38 +1,36 @@
 #include "main.h"
 
 /**
- *  _strspn - a function that gets the length of a prefix substring.
- *  @s: the first pointer .
- *  @accept: the second pointer .
+ * _strspn - a function that gets the length.
+ * @s: the first pointer .
+ * @accept: the second pointer .
  *
- *  Description: a function that gets the length of a prefix substring.
- *  Return: the initial segment of s.
+ * Description: a function that gets the length
+ * of a prefix substring.
+ * Return: the initial segment of s.
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int i;
-	unsigned int val = 0;
+int i;
+unsigned int val = 0;
 
-	while (*s != '\0')
-	{
-		for (i = 0; accept[i] != '\0'; i++)
-		{
-			if (*s == accept[i])
-			{
-				val++;
-				break;
-			}
-
-		        else if (accept[i + 1] == '\0')
-                        {
-                                return (val);
-                        }
-		}
-		s++;
-
-	}
-	return (val);
-
+while (*s != '\0')
+{
+for (i = 0; accept[i] != '\0'; i++)
+{
+if (*s == accept[i])
+{
+val++;
+break;
+}
+else if (accept[i + 1] == '\0')
+{
+return (val);
+}
+}
+s++;
+}
+return (val);
 
 }
