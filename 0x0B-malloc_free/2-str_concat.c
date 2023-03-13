eddef84d7823588efeb1cecb31920bf2aee90e68
MohamedElKh <mohamedkharashy27@yahoo.com>
2023-03-13 04:44:38 -0700

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,67 +13,59 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int x, a, r, n;
-	char *name;
+int x, a, r, n;
+char *name;
 
-	x = 0;
-	a = 0;
-	n = 0;
-	r = 0;
+x = 0;
+a = 0;
+n = 0;
+r = 0;
 
-	if (s1 == NULL)
-	{
-		s1 = "";
-	}
-	else
-	{
-		while (s1[x] != '\0')
-		{
-			x++;
-		}
-	}
-
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
-	else
-	{
-		while (s2[a] != '\0')
-		{
-			a++;
-		}
-	}
-
-	r = x + a;
-
-	name = malloc((r * sizeof(char)) + 1);
-
-
-
-	if (name == NULL)
-	{
-		return (NULL);
-	}
-	else
-	{
-		a = 0;
-
-		while (n < r)
-		{
-			if (n <= x)
-			{
-				name[n] = s1[n];
-			}
-			 if (n >= x)
-			{
-				name[n] = s2[a];
-				a++;
-			}
-			n++;
-		}
-
-	}
-	name[n] = '\0';
-	return (name);
+if (s1 == NULL)
+{
+s1 = "";
+}
+else
+{
+while (s1[x] != '\0')
+{
+x++;
+}
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
+else
+{
+while (s2[a] != '\0')
+{
+a++;
+}
+}
+r = x + a;
+name = malloc((r * sizeof(char)) + 1);
+if (name == NULL)
+{
+return (NULL);
+}
+else
+{
+a = 0;
+while (n < r)
+{
+if (n <= x)
+{
+name[n] = s1[n];
+}
+if (n >= x)
+{
+name[n] = s2[a];
+a++;
+}
+n++;
+}
+}
+name[n] = '\0';
+return (name);
 }
