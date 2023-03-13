cfc1d8b5f7e25f047c29f3c12a84e60934856da3
MohamedElKh <mohamedkharashy27@yahoo.com>
2023-03-13 06:27:20 -0700

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,39 +13,35 @@
 
 char *str_concat(char *s1, char *s2)
 {
-  int a, x, z, y;
-  char *name;
+int a, x, z, y;
+char *name;
+a = 0, x = 0, z = 0, y = 0;
 
-  a = 0, x = 0, z = 0, y = 0;
+if (s1 == NULL)
+{
+s1 = "";
+}
+else if (s1 != NULL)
+{
+while(s1[a] != '\0')
+a++;
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
+else if (s2 != NULL)
+{
+while(s2[x] != '\0')
+x++;
+}
+z = a + x;
+name = malloc((sizeof(char) * z) + 1);
 
-  if (s1 == NULL)
-  {
-	  s1 = "";
-  }
-  else if (s1 != NULL) 
-  {
-	  while(s1[a] != '\0')
-		a++;
-  }
+if (name == NULL)
+return (NULL);
+x = 0;
 
-  if (s2 == NULL)
-  {
-	  s2 = "";
-  }
-  else if (s2 != NULL)
-  {
-	  while(s2[x] != '\0')
-		  x++;
-  }
-
-  z = a + x;
-  name = malloc((sizeof(char) * z) + 1);
-
-  if (name == NULL)
-	  return (NULL);
-
-  x = 0;
-	
   while (y < z)
   {
 	  if (y <= a)
