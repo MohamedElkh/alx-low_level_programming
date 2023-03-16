e2f89409048db36bb5ba3589120efad2b1a83c61
MohamedElKh <mohamedkharashy27@yahoo.com>
2023-03-16 06:29:14 -0700

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,57 +1,56 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
-  * string_nconcat -  a function that concatenates two strings.
-  * @s1: pointer to be checked
-  * @s2: pointer to be checked
-  * @n: character to be checked
-  *
-  * Description: a function that concatenates two strings.
-  * Return: the result
-  */
+ * string_nconcat - a function that concatenates two strings.
+ * @s1: the pointer to be checked.
+ * @s2: the second pointer to be checked.
+ * @n: the character to be check.
+ *
+ * Description: a function that concatenates two strings.
+ * Return: the result.
+ */
+
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int x = 0, p = 0, a = 0, y = 0;
-        char *val;
+	unsigned int x = 0, a = 0, j = 0, z = 0;
+	char *val;
 
-        if (s1 == NULL)
-                s1 = "";
-        if (s2 == NULL)
-                s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-        while (s1[x])
-                x++;
+	while (s1[x] != '\0')
+		x++;
 
-        while (s2[a])
-                a++;
+	while (s2[a] != '\0')
+		a++;
 
-        if (n >= a)
-                y = x + a;
-        else
-                y = x + n;
+	if (n >= a)
+		j = x + a;
+	else
+		j = x + n;
 
-        val = malloc(sizeof(char) * y + 1);
-        if (val == NULL)
-                return (NULL);
+	val = malloc(sizeof(char) * j + 1);
+	if (str == NULL)
+		return (NULL);
 
-        a = 0;
-        while (p < y)
-        {
-                if (p <= x)
-                        val[p] = s1[p];
+	a = 0;
+	while (z < x)
+	{
+		if (z <= x)
+			str[z] = s1[z];
 
-                if (p >= x)
-                {
-                        val[p] = s2[a];
-                        a++;
-                }
-                p++;
-        }
-        val[p] = '\0';
-        return (val);
+		if (z >= x)
+		{
+			val[z] = s2[a];
+			a++;
+		}
+		z++;
+	}
+	val[z] = '\0';
+	return (val);
 }
-
-
-
