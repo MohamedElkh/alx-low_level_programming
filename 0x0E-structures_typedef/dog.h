14eb74184e96b38d352b44c44b57b3f867515fb7
MohamedElKh <mohamedkharashy27@yahoo.com>
2023-03-20 07:40:15 -0700

    0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,7 +12,7 @@
 struct dog
 {
 	char * name;
-	float age;
+	float  age;
 	char * owner;
 };
 
@@ -20,6 +20,7 @@ struct dog
  * dog_t - it is a short name for struck dog
  */
 typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
