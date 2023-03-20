a26a1f011460d72e12de4bfa389e94fbdb88d859
MohamedElKh <mohamedkharashy27@yahoo.com>
2023-03-20 07:26:01 -0700

    0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef _NAME_
-#define _NAME_
+#ifndef DOG_H
+#define DOG_H
 
 struct dog
 {
@@ -13,7 +13,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
 #endif
 
 
