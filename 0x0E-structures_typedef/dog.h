d56d288d3e331eef60fab6e9993f032038d09849
MohamedElKh <mohamedkharashy27@yahoo.com>
2023-03-20 07:30:06 -0700

    0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,21 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struck dog - it type of functions
+ * @name: string
+ * @age: float
+ * @owner: string
+ *
+ * Description: its a struck
+ */
+
 struct dog
 {
 	char * name;
 	float age;
 	char * owner;
 };
-
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
