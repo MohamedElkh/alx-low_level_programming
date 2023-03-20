7a61f563aaba149ebbdec778c263be79bdf630fd
MohamedElKh <mohamedkharashy27@yahoo.com>
2023-03-20 02:42:54 -0700

    0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _NAME_
+#define _NAME_
+
+struct dog
+{
+	char * name;
+	float age;
+	char * owner;
+};
+
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
+
+
