3cb9c73a9b1d34fda14f291ac998a4f8e07a809a
MohamedElKh <mohamedkharashy27@yahoo.com>
2023-03-30 03:52:13 -0700

    0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,21 +9,20 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t value = 0;
+size_t value = 0;
 
-	while (h != NULL)
-	{
-		if (h->str == NULL)
-		{
-			printf("[0] (nil)\n");
-		}
-		else
-		{
-			printf("[%d] %s\n", h->len, h->str);
-		}
-		h = h->next;
-
-		value++;
-	}
-	return (value);
+while (h != NULL)
+{
+if (h->str == NULL)
+{
+printf("[0] (nil)\n");
+}
+else
+{
+printf("[%d] %s\n", h->len, h->str);
+}
+h = h->next;
+value++;
+}
+return (value);
 }
