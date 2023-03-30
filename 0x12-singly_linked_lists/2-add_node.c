4fc0e04d5f8d655e24399a91154d98903ef25da5
MohamedElKh <mohamedkharashy27@yahoo.com>
2023-03-30 06:34:20 -0700

    2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -34,7 +34,7 @@ list_t *add_node(list_t **head, const char *str)
 
 	if (head != NULL && str != NULL)
 	{
-		value = (list_t*) malloc(sizeof(list_t));
+		value = (list_t *)malloc(sizeof(list_t));
 		if (value == NULL)
 			return (NULL);
 
