c0cdd35c08d4d38b12af44bb8bced94a75215ce7
MohamedElKh <mohamedkharashy27@yahoo.com>
2023-04-08 02:55:33 -0700

    2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * _count - function to count the length
+ * @n: the character to be checked.
+ *
+ * Return: the result.
+ */
+
+int _count(char *n)
+{
+	int x = 0;
+
+	while (n[x] != '\0')
+	{
+		x++;
+	}
+	return (x);
+}
+
+/**
+ * append_text_to_file - function that appends text at the end.
+ * @filename: the character to be checked
+ * @text_content: the content to be checked.
+ *
+ * Return: the result.
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fx;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	fx = open(filename, O_WRONLY | O_APPEND);
+
+	if (fx == -1)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		if (write(fx, text_content, _count(text_content)) == -1)
+		{
+			return (-1);
+		}
+	}
+	close (fx);
+	return (1);
+
+}
