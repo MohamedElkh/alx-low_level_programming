893474a2da483561d4b0098e606a63451ba80e37
MohamedElKh <mohamedkharashy27@yahoo.com>
2023-04-08 02:34:37 -0700

    0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,7 +13,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *value = malloc(sizeof(char) * letters);
 	int x, rea, wri;
 
-	if (!filename)
+	if (filename == NULL)
 	{
 		return (0);
 	}
---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _count - function to count the length
+ * @n: the character to be checked.
+ *
+ * Return: the result.
+ */
+
+int _count(char *n)
+{
+	int x = 0;
+
+	while (n[x] != '\0')
+	{
+		x++;
+	}
+	return (x);
+}
+
+/**
+ * create_file - a function that creates a file.
+ * @filename: the character to be checked
+ * @text_content: the character to be checked.
+ *
+ * Return: the result.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int 
+
+}
