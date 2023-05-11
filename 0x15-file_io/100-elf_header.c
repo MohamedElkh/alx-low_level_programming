244ed0333fd59a56a5ed4df5b025f8a14db701ee
mohamed <mohamedkharashy27@yahoo.com>
2023-05-11 00:09:26 -0700

    1

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -285,11 +285,12 @@ void p_entry(unsigned long int e_entry, unsigned char *e_ident)
 
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	Elf64_Ehdr *header;
-
 	int xx, i;
 
+	Elf64_Ehdr *header;
+
 	xx = open(argv[1], O_RDONLY);
+
 	if (xx  == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
